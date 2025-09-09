5fc40a45b44385f6bc1e37f14f39efe6283d9737
Aliq8p <48098219+Aliq8p@users.noreply.github.com>
2025-09-09 12:28:02 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,7 @@ export const metadata = {
   title: "لعبة الأسئلة والأجوبة",
   description: "نسخة ويب بسيطة للعبة الأسئلة والأجوبة بالعربي",
 };
-
 import "./globals.css";
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
